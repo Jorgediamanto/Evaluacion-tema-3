9e48f5f2f974faeedc4b38aa3d754b0b121bd821
Jorgediamanto <99408709+Jorgediamanto@users.noreply.github.com>
2022-10-28 11:53:43 +0200

    Add files via upload

--- lanzador.py
+++ lanzador.py
@@ -1,1 +1,17 @@
 import sys
+
+def lanzador():
+    a = int(input("Que apartado desea resolver? \n Ejercicio 1 \n Ejercicio 2: "))
+
+    if a == 1:
+        import ejercicios.ev3ej1
+        lanzador()
+
+    elif a == 2:
+        import ejercicios.ev3ej2
+        lanzador
+
+    else:
+        sys.exit()
+
+lanzador()
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 from lanzador import lanzador
+
+if __name__ == "__main__":
+    lanzador()
